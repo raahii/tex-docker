31e53a0ace802170c7b4be09364a46c9b26fed59
raahii <piyo56.net@gmail.com>
2019-09-01 16:09:40 +0900

    add start docker function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/raahii/tex-on-docker
 
 go 1.12
 
-require github.com/jessevdk/go-flags v1.4.0
+require (
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- start.go
+++ start.go
@@ -1,23 +1,62 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"errors"
+	"fmt"
+	"os/exec"
 
 	"github.com/jessevdk/go-flags"
 )
 
+var (
+	dockerImage string = "tex-on-docker"
+)
+
 type arguments struct {
-	Path string `short:"p" long:"path" required:"true" description:"latex source directory to compile"`
+	Path          string `short:"p" long:"path" required:"true" description:"latex source directory to compile"`
+	ContainerName string `short:"n" long:"container-name" description:"docker container name"`
 }
 
-func startDocker(args *arguments) {
+func startDocker(args *arguments) (string, error) {
+	// build commands
+	cmds := []string{"docker", "run", "-d"}
+
+	// volume option
+	volume := fmt.Sprintf("%s:/home/work", args.Path)
+	cmds = append(cmds, "--volume")
+	cmds = append(cmds, volume)
+
+	// name option
+	if len(args.ContainerName) > 0 {
+		cmds = append(cmds, "--name")
+		cmds = append(cmds, args.ContainerName)
+	}
+
+	// docker image name
+	cmds = append(cmds, dockerImage)
+
+	// build command
+	cmd := exec.Command(cmds[0], cmds[1:]...)
+	stderr := &bytes.Buffer{}
+	cmd.Stderr = stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return string(out), errors.New(stderr.String())
+	}
+	return string(out), nil
 }
 
 func main() {
 	var args arguments
 	if _, err := flags.Parse(&args); err != nil {
-		os.Exit(1)
+		return
 	}
 
-	startDocker(&args)
+	out, err := startDocker(&args)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(out)
 }
